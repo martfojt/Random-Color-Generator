eb2f4e382b11ed0dc717454ee68243f27022442b
Martin Fojtek <martfojt@gmail.com>
2024-01-09 20:38:22 +0100

    Add code for random color generator

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 import chalk from 'chalk';
+import { argv } from 'process';
+import randomColor from 'randomColor';
 
-console.log(chalk.blue('Hello world!'));
+function generateColor(color) {
+  const block = `
+###############################
+###############################
+###############################
+#######     ${color}     #######
+###############################
+###############################
+###############################
+`;
+  return chalk.hex(color)(block);
+}
+
+const hue = argv[2];
+const luminosity = argv[3];
+
+const color = randomColor({ hue: hue, luminosity: luminosity });
+
+console.log(generateColor(color));
